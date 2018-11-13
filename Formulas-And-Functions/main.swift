480e6e1c729f2dd3e3ab562d2e9d457f7776c295
Gavin Plunkett <gplunkett@lcs.on.ca>
2018-11-13 14:51:50 -0500

    Example of difference between constant and variable.

--- Formulas-And-Functions/main.swift
+++ Formulas-And-Functions/main.swift
@@ -9,6 +9,16 @@
 import Foundation
 
 // Find the area of a square, using a function
-let a = 7.0
+let a = 9.0  //"a" is a constant (let)
 let areaOfSquare = forSquareWith(sideLength: a)
 print("The area of a square with side length \(a) is \(areaOfSquare)")
+
+//a = 8  This cannot happen. "a" is a constant
+
+//example of using a variable
+var b = 17.0
+b=18.0
+//how to print a variable to the screen
+//\(b) ... this is the called string interpolation
+print("The value of b is \(b)")
+
